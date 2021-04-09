34aa90d3dc5cc4a366017a5c100e31f55791fc34
wei756 <35715109+wei756@users.noreply.github.com>
2021-04-09 23:39:06 +0900

    #29 모바일 활동정지 여부 표시 안 되는 버그 수정
    html 내부에 script 태그로 스크립트를 삽입하여 CORS 우회함
    
    Resolve #29

--- source/content_script_mobile.js
+++ source/content_script_mobile.js
@@ -4,39 +4,67 @@
  */
 
 jQuery(function($){
-    $(document).ready(function() {
+    $(document).ready(() => {
         checkActivityStop();
     });
 
     /**
-     * 회원이 활동정지 상태인지 확인하고 그 상태를 출력합니다.
+     * @description 현재 보고 있는 회원이 활동정지 상태인지 확인하고 그 상태를 프로필에 표시합니다.
      */
     function checkActivityStop() {
-        if (location.href.indexOf("CafeMemberProfile.nhn") != -1) {
-            // 프로필 페이지 로딩
-            var params = getURLParams();
-            var cafeId = params["cafeId"];
-            var memberId = params["memberId"];
-            if (memberId) {
-                $.ajax({
-                    type: "POST",
-                    url: "https://apis.naver.com/cafe-web/cafe-mobile/CafeMemberStatus?cafeId=" + cafeId + "&memberId=" + memberId,
-                    dataType: "json",
+        if (location.href.indexOf('CafeMemberProfile.nhn') !== -1) { // 프로필 페이지
+            const script = `
+            <script type="text/javascript">
+            // injected by NaverCafe-Addon
+            // for checkActivityStop
+            function getURLParams() {
+                const params = [];
+                try {
+                    const url = decodeURIComponent(decodeURIComponent(location.href));
+                    const paramsStr = url.substring( url.indexOf('?')+1, url.length ).split("&");
+                    paramsStr.map(param => {
+                        const [k, v] = param.split("=");
+                        params[k] = v;
+                    });
+                } catch(err) {
+                    console.error(err);
+                }
+                
+                return params;
+            }
+            function getActivityStopMobile({ cafeid, memberid, callback }) {
+                jQuery.ajax({
+                    type: 'POST',
+                    url: 'https://apis.naver.com/cafe-web/cafe-mobile/CafeMemberStatus?cafeId=' + cafeid + '&memberId=' + memberid,
+                    dataType: 'json',
                     xhrFields: {
                         withCredentials: true
                     },
                     crossDomain: true,
-                    success:function(data){
-                        if (data.message.status == "200" && data.message.result.activityStop) {
-                            var stoppedElement = document.createElement("span");
-                            stoppedElement.append(" (활동 정지됨)");
-                            document.querySelector(".profile_head strong.nick").appendChild(stoppedElement);
-                        }
+                    success: data => {
+                        callback(data.message.status == '200' && data.message.result.activityStop);
                     },
-                    error: function (xhr) {
+                    error: xhr => {
+                        callback(false);
                     }
-                })
+                });
             }
+            jQuery(document).ready(() => {
+                const params = getURLParams();
+                const { cafeId, memberId } = params;
+                getActivityStopMobile({
+                    cafeid: cafeId, 
+                    memberid: memberId,
+                    callback: stopped => {
+                        if (stopped) {
+                            document.querySelector('.profile_head strong.nick').innerHTML += '<span class="id">(활동 정지됨)</span>';
+                        }
+                    }
+                });
+            });
+            </script>`;
+            $('body').append($(script));
+            // CORS 문제로 html에 삽입하여 실행되는 형태로 구현함
         }
     }
 
